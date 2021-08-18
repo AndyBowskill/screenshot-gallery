8756fdda80cc48b3f1b51ff0f228698600dab08a
Andy Bowskill <AndyBowskill@users.noreply.github.com>
2021-08-18 15:11:59 +0100

    Added a navigation component to a website.

---
+++ src/components/Navigation.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Navigation.style.css';
+
+const Navigation = () => {
+    return (
+        <nav className='navigation'>Sign Out</nav>
+    )
+}
+
+export default Navigation;
---
+++ src/containers/App.js
@@ -0,0 +1,11 @@
+import Navigation from "../components/Navigation.component";
+
+function App() {
+  return (
+    <div>
+      <Navigation />
+    </div>
+  );
+}
+
+export default App;
